8446e454d75a85902fcf06aae77917cfebed6ce9
James Whelton <james@whelton.io>
2021-01-16 16:10:19 +0000

    Fix Autoloading

--- lib/railgun/railtie.rb
+++ lib/railgun/railtie.rb
@@ -4,7 +4,9 @@ module Railgun
   class Railtie < ::Rails::Railtie
     # See https://github.com/mailgun/mailgun-ruby/pull/162
     initializer 'railgun.configure_rails_initialization', before: 'action_mailer.set_configs' do
-      ActionMailer::Base.add_delivery_method(:mailgun, Railgun::Mailer)
+      ActiveSupport.on_load :action_mailer do
+        ActionMailer::Base.add_delivery_method(:mailgun, Railgun::Mailer)
+      end
     end
   end
 end
